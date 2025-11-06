85093e778631e5c154c67e16f59c953408484449
Keon Oh Kim <keonoh00@gmail.com>
2025-11-06 09:57:29 +0900

    enh: resolve comments from copilot

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,23 +3,21 @@ import seed from "./data.json";
 
 const prisma = new PrismaClient();
 async function main() {
-  for (const attack of seed) {
-    await prisma.ability.create({
-      data: {
-        ability_id: attack.id,
-        ability_name: attack.ability_name,
-        tactic: attack.tactic,
-        technique_id: attack.technique_id,
-        technique_name: attack.technique_name,
-        payload: attack.payloads,
-        platform: attack.platform,
-        command: attack.command,
-        shell_type: attack.shell_type,
-        type: attack.type,
-        description: attack.description ?? "",
-      },
-    });
-  }
+  await prisma.ability.createMany({
+    data: seed.map((attack) => ({
+      ability_id: attack.id,
+      ability_name: attack.ability_name,
+      tactic: attack.tactic,
+      technique_id: attack.technique_id,
+      technique_name: attack.technique_name,
+      payload: attack.payloads,
+      platform: attack.platform,
+      command: attack.command,
+      shell_type: attack.shell_type,
+      type: attack.type,
+      description: attack.description ?? "",
+    })),
+  });
 }
 
 main()
