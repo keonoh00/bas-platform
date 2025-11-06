eac75a8156519df73332d03351cf17628144cb96
Keon Oh Kim <keonoh00@gmail.com>
2025-11-06 13:41:46 +0900

    enh: remove legacy api

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://192.168.5.111:8888/:path*",
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
